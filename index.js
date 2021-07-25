227138e4b64742e6537131f7fd7a4ce9bb1d6d5c
mrPrateek95 <gajprateek95@gmail.com>
2021-07-25 16:39:39 +0530

    Implemented the code to process a single template

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const LG = require('botbuilder-lg')
 
-const lgResource = new LG.LGResource("test", "test", "# HelloWorld \r\n - Hello World!")
+const templateName = "TemplateToTest"
+
+// Pass from Template section of the UI
+const template = "- Hello ${name}!";
+
+// Pass from Object / JSON section of the UI
+const objectToPass = {
+    name: "Varada"
+};
+
+const lgResource = new LG.LGResource(null, null, `# ${templateName} \r\n ${template}`)
 
 const generatedTemplates = LG.Templates.parseResource(lgResource)
 
-console.log(generatedTemplates.evaluate("HelloWorld"));
+// Display in the result section
+console.log(generatedTemplates.evaluate(templateName, objectToPass));
