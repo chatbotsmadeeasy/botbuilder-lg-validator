372a3717647052a970eafda97736f9f7b8b1d059
mrPrateek95 <gajprateek95@gmail.com>
2021-07-25 17:33:01 +0530

    Used browserify to bundle JS

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ const LG = require('botbuilder-lg')
 
 const templateName = "TemplateToTest"
 
-// Pass from Template section of the UI
-const template = "- Hello ${name}!";
+window.validateLG = function(fnTemplate, fnObject) {
+    const lgResource = new LG.LGResource(null, null, `# ${templateName} \r\n ${fnTemplate}`)
 
-// Pass from Object / JSON section of the UI
-const objectToPass = {
-    name: "Varada"
-};
+    const generatedTemplates = LG.Templates.parseResource(lgResource)
 
-const lgResource = new LG.LGResource(null, null, `# ${templateName} \r\n ${template}`)
-
-const generatedTemplates = LG.Templates.parseResource(lgResource)
-
-// Display in the result section
-console.log(generatedTemplates.evaluate(templateName, objectToPass));
+    return generatedTemplates.evaluate(templateName, fnObject)
+}
